171aa95ab94eef26b8ab00ff8ba0825a0c2cc48d
Mochamad Faishal <mochamadfaishal1311@gmail.com>
2021-07-19 05:40:49 +0700

    Add files via upload

---
+++ CRUD/connection.py
@@ -0,0 +1,12 @@
+import mysql.connector
+
+#connect to mysql-server with mysql-connector (using xampp)
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd=""
+)
+
+#if succesfully connected
+if db.is_connected():
+  print("Terhubung dengan Database")
---
+++ CRUD/create_db.py
@@ -0,0 +1,14 @@
+import mysql.connector
+
+#connect to mysql-server with mysql-connector (using xampp)
+db = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd=""
+)
+
+#create new database
+cursor = db.cursor()
+cursor.execute("CREATE DATABASE toko_super")
+
+print("Database berhasil dibuat!")
